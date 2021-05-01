5991e5d5b5e5920c606af2fa6679bd49718acee8
ttchalakov <teodor.tch@gmail.com>
2021-05-01 15:12:31 -0500

    Setup geometry 2d and 3d packages with vectors

---
+++ lib/src/test/java/com/titanrobotics2022/geometry/geometry2d/Vector2DTest.java
@@ -0,0 +1,12 @@
+package com.titanrobotics2022.geometry.geometry2d;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class Vector2DTest {
+    @Test
+    public void assertTrueTest() {
+        assertTrue(true);
+    }
+}
