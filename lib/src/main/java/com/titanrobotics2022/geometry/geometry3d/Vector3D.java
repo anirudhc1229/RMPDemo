b4f0e38c0f100bdb375b7acd5a362d4aa40062f6
ttchalakov <teodor.tch@gmail.com>
2021-05-01 17:27:14 -0500

    Began vector 3d classes with keyable design

--- lib/src/main/java/com/titanrobotics2022/geometry/geometry3d/Vector3D.java
+++ lib/src/main/java/com/titanrobotics2022/geometry/geometry3d/Vector3D.java
@@ -1,5 +1,31 @@
 package com.titanrobotics2022.geometry.geometry3d;
 
+/**
+ * A vector 3D class
+ */
 public class Vector3D {
-    
+    public final double x, y, z;
+
+    /**
+     * Creates a 3D cartesian vector
+     * @param x
+     * @param y
+     * @param z
+     */
+    public Vector3D(double x, double y, double z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    /**
+     * Adds two vectors together
+     * @param rhs the vector to be added
+     * @return the resulting vector
+     */
+    public Vector3D plus(Vector3D rhs)
+    {
+        return new Vector3D(this.x + rhs.x, this.y + rhs.y, this.z + rhs.z);
+    }
 }
--- lib/src/test/java/com/titanrobotics2022/geometry/geometry3d/Vector3DTest.java
+++ lib/src/test/java/com/titanrobotics2022/geometry/geometry3d/Vector3DTest.java
@@ -6,7 +6,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Vector3DTest {
     @Test
-    public void assertTrueTest() {
-        assertTrue(true);
+    public void additionTest() {// TODO: edit to incorporate double precision error
+        Vector3D a = new Vector3D(1, 1, 1);
+        Vector3D b = new Vector3D(2, 3, 4);
+        Vector3D actual = a.plus(b);
+        assertTrue(actual.x == 1 + 2);
+        assertTrue(actual.y == 1 + 3);
+        assertTrue(actual.z == 1 + 4);
     }
 }
