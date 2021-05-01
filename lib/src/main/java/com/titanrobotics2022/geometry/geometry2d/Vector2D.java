26269f0539abd038c4936ea10de2e671cc09f4a7
ttchalakov <teodor.tch@gmail.com>
2021-05-01 17:27:06 -0500

    Began vector 2d classes with keyable design

--- lib/src/main/java/com/titanrobotics2022/geometry/geometry2d/Vector2D.java
+++ lib/src/main/java/com/titanrobotics2022/geometry/geometry2d/Vector2D.java
@@ -1,5 +1,29 @@
 package com.titanrobotics2022.geometry.geometry2d;
 
+/**
+ * A vector 2D class
+ */
 public class Vector2D {
-    
+    public final double x, y;
+
+    /**
+     * Creates a cartesian vector 
+     * @param x component
+     * @param y component
+     */
+    public Vector2D(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Adds two vectors together
+     * @param rhs the vector to be added
+     * @return the resulting vector
+     */
+    public Vector2D plus(Vector2D rhs)
+    {
+        return new Vector2D(this.x + rhs.x, this.y + rhs.y);
+    }
 }
--- lib/src/test/java/com/titanrobotics2022/geometry/geometry2d/Vector2DTest.java
+++ lib/src/test/java/com/titanrobotics2022/geometry/geometry2d/Vector2DTest.java
@@ -1,12 +1,16 @@
 package com.titanrobotics2022.geometry.geometry2d;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Vector2DTest {
     @Test
-    public void assertTrueTest() {
-        assertTrue(true);
+    public void additionTest() {// TODO: edit to incorporate double precision error
+        Vector2D a = new Vector2D(1, 1);
+        Vector2D b = new Vector2D(2, 3);
+        Vector2D actual = a.plus(b);
+        assertTrue(actual.x == 1 + 2);
+        assertTrue(actual.y == 1 + 3);
     }
 }
