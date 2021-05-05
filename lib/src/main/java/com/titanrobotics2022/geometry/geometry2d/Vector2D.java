935dd784d2723f66a37bd3b3080e1082c94420ab
ttchalakov <teodor.tch@gmail.com>
2021-05-04 21:38:06 -0500

    Wrote basic Vector2D class with unit tests

--- lib/src/main/java/com/titanrobotics2022/geometry/geometry2d/Vector2D.java
+++ lib/src/main/java/com/titanrobotics2022/geometry/geometry2d/Vector2D.java
@@ -1,13 +1,31 @@
 package com.titanrobotics2022.geometry.geometry2d;
 
+import org.apache.commons.math3.util.FastMath;
+
 /**
  * A vector 2D class
  */
 public class Vector2D {
-    public final double x, y;
+    /** Origin (coordinates: 0, 0). */
+    public static final Vector2D ZERO = new Vector2D(0, 0);
+
+    /** A vector with all coordinates set to NaN. */
+    public static final Vector2D NAN = new Vector2D(Double.NaN, Double.NaN);
+
+    /** A vector with all coordinates set to positive infinity. */
+    public static final Vector2D POSITIVE_INFINITY = new Vector2D(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
+
+    /** A vector with all coordinates set to negative infinity. */
+    public static final Vector2D NEGATIVE_INFINITY = new Vector2D(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY);
+    
+    /** The x component. */
+    public final double x;
+
+    /** The y componenet. */
+    public final double y;
 
     /**
-     * Creates a cartesian vector 
+     * Creates a cartesian vector. 
      * @param x component
      * @param y component
      */
@@ -25,5 +43,165 @@ public class Vector2D {
     public Vector2D plus(Vector2D rhs)
     {
         return new Vector2D(this.x + rhs.x, this.y + rhs.y);
+    }
+
+    /**
+     * Subtracts two vectors
+     * @param rhs rhs the vector to be subtracted
+     * @return the resulting vector
+     */
+    public Vector2D minus(Vector2D rhs)
+    {
+        return new Vector2D(this.x - rhs.x, this.y - rhs.y);
+    }
+
+    /**
+     * 
+     * @param scalar
+     * @return
+     */
+    public Vector2D scalarMultiply(double scalar)
+    {
+        return new Vector2D(this.x * scalar, this.y * scalar);
+    }
+
+    /**
+     * 
+     * @param divisor
+     * @return
+     */
+    public Vector2D scalarDivide(double divisor)
+    {
+        return new Vector2D(this.x / divisor, this.y / divisor);
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public Vector2D negate()
+    {
+        return new Vector2D(-x, -y);
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public double magnitude()
+    {
+        return FastMath.sqrt(x * x + y * y);
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public double magnitudeSquared()
+    {
+        return x * x + y * y;
+    }
+
+
+    /**
+     * The azimuthal angle is the angle from the x axis in the x-y plane (phi in physics, theta in math)
+     * @return
+     */
+    public double azimuthalAngle()
+    {
+        double answer = FastMath.atan2(y, x);
+        if (answer < 0)
+            answer += 2 * Math.PI;
+        return answer;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public Vector2D unitVector()
+    {
+        double mag2 = magnitudeSquared();
+        if (mag2 == 0)
+        {
+            return ZERO;
+        }
+        else
+        {
+            return scalarDivide(FastMath.sqrt(mag2));
+        }
+    }
+
+    /**
+     * 
+     * @param rhs
+     * @return
+     */
+    public double dot(Vector2D rhs)
+    {
+        return x * rhs.x + y * rhs.y;
+    }
+
+    /**
+     * 
+     * @param rhs
+     * @return
+     */
+    public double cross(Vector2D rhs)
+    {
+        return x * rhs.y - rhs.x * y;
+    }
+
+    /**
+     * 
+     * @param otherVec
+     * @return
+     */
+    public Vector2D projectOnto(Vector2D otherVec)
+    {
+        Vector2D unitVec = otherVec.unitVector();
+        return unitVec.scalarMultiply(dot(unitVec));
+    }
+
+    /**
+     * 
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public static double angleBetween(Vector2D v1, Vector2D v2)
+    {
+        return FastMath.acos(v1.dot(v2) / (v1.magnitude() * v2.magnitude()));
+    }
+
+    public static Vector2D polarVector(double r, double phi)
+    {
+        double x = r * FastMath.cos(phi);
+        double y = r * FastMath.sin(phi);
+        return new Vector2D(x, y);
+    }
+
+    /**
+     * 
+     */
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj instanceof Vector2D) {
+            final Vector2D rhs = (Vector2D) obj;
+            return (x == rhs.x) && (y == rhs.y);
+        }
+        return false;
+    }
+
+    /**
+     * 
+     * @param rhs
+     * @param tolerance
+     * @return
+     */
+    public boolean equals(Vector2D rhs, double tolerance)
+    {
+        return FastMath.abs(x - rhs.x) < tolerance && FastMath.abs(y - rhs.y) < tolerance;
     }
 }
--- lib/src/test/java/com/titanrobotics2022/geometry/geometry2d/Vector2DTest.java
+++ lib/src/test/java/com/titanrobotics2022/geometry/geometry2d/Vector2DTest.java
@@ -2,15 +2,156 @@ package com.titanrobotics2022.geometry.geometry2d;
 
 import org.junit.jupiter.api.Test;
 
+import edu.wpi.first.wpiutil.math.Vector;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.apache.commons.math3.util.FastMath;
+
 public class Vector2DTest {
+    private static final double delta = 1e-9;
     @Test
-    public void additionTest() {// TODO: edit to incorporate double precision error
+    void additionTest() {// TODO: edit to incorporate double precision error
         Vector2D a = new Vector2D(1, 1);
         Vector2D b = new Vector2D(2, 3);
         Vector2D actual = a.plus(b);
-        assertTrue(actual.x == 1 + 2);
-        assertTrue(actual.y == 1 + 3);
+        assertEquals(1 + 2, actual.x, delta);
+        assertEquals(1 + 3, actual.y, delta);
+    }
+    @Test
+    void subtractionTest() {// TODO: edit to incorporate double precision error
+        Vector2D a = new Vector2D(1, 1);
+        Vector2D b = new Vector2D(2, 3);
+        Vector2D actual = a.minus(b);
+        assertEquals(1 - 2, actual.x, delta);
+        assertEquals(1 - 3, actual.y, delta);
+    }
+    @Test
+    void scalarMultiplicationTest() {// TODO: edit to incorporate double precision error
+        final double scalar = 2.4;
+        Vector2D a = new Vector2D(2.5, 3.5);
+        Vector2D actual = a.scalarMultiply(scalar);
+        assertEquals(2.5 * scalar, actual.x, delta);
+        assertEquals(3.5 * scalar, actual.y, delta);
+    }
+    @Test
+    void scalarDivisionTest() {// TODO: edit to incorporate double precision error
+        final double divisor = 2.4;
+        Vector2D a = new Vector2D(2.5, 3.5);
+        Vector2D actual = a.scalarDivide(divisor);
+        assertEquals(2.5 / divisor, actual.x, delta);
+        assertEquals(3.5 / divisor, actual.y, delta);
+    }
+    @Test
+    void negationTest()
+    {
+        Vector2D a = new Vector2D(1, -1);
+        Vector2D actual = a.negate();
+        assertEquals(-1, actual.x);
+        assertEquals(1, actual.y);
+    }
+    @Test
+    void magnitudeTest()
+    {
+        Vector2D a = new Vector2D(2, 2);
+        double actual = a.magnitude();
+        assertEquals(Math.sqrt(2 * 2 + 2 * 2), actual, delta); 
+    }
+    @Test
+    void magnitudeSquaredTest()
+    {
+        Vector2D a = new Vector2D(2, 2);
+        double actual = a.magnitudeSquared();
+        assertEquals(2 * 2 + 2 * 2, actual, delta); 
+    }
+    @Test
+    void azimuthalAngleTest()
+    {
+        Vector2D positiveX = new Vector2D(2, 2);
+        Vector2D negativeX = new Vector2D(-2, 2);
+        Vector2D alongPositiveX = new Vector2D(2,0);
+        double actual = positiveX.azimuthalAngle();
+        assertEquals(Math.PI / 4.0, actual, delta);
+        actual = negativeX.azimuthalAngle();
+        assertEquals(3 * Math.PI / 4.0, actual, delta);
+        actual = alongPositiveX.azimuthalAngle();
+        assertEquals(0, actual, delta);
+    }
+    @Test
+    void unitVectorTest()
+    {
+        Vector2D zeroVector = new Vector2D(0, 0);
+        Vector2D a = new Vector2D(-2, 2);
+        Vector2D actual = zeroVector.unitVector();
+        assertTrue(Vector2D.ZERO == actual); // Comparing object references
+        actual = a.unitVector();
+        double magnitude = FastMath.sqrt(-2 * -2 + 2 * 2);                 
+        Vector2D expected = new Vector2D(-2 / magnitude, 2 / magnitude);
+        assertTrue(actual.equals(expected, delta));
+    }
+    @Test
+    void dotProductTest()
+    {
+        Vector2D a = new Vector2D(2, 3);
+        Vector2D b = new Vector2D(-3, 3);
+        double actual = a.dot(b);
+        assertEquals(2 * -3 + 3 * 3, actual, delta);
+    }
+    @Test
+    void crossProductTest()
+    {
+        Vector2D a = new Vector2D(2, 3);
+        Vector2D b = new Vector2D(-3, 3);
+        double actual = a.cross(b);
+        assertEquals(2 * 3 - (-3 * 3), actual, delta);
+    }
+    @Test
+    void projectionTest() // TODO: Implement
+    {
+        Vector2D a = new Vector2D(1, 1);
+        Vector2D b = new Vector2D(2, 0);
+        Vector2D longerB = new Vector2D(10, 0);
+        Vector2D actual = a.projectOnto(b);
+        assertEquals(1, actual.x, delta);
+        assertEquals(0, actual.y, delta);
+        actual = a.projectOnto(longerB);
+        assertEquals(1, actual.x, delta);
+        assertEquals(0, actual.y, delta);
+    }
+    @Test
+    void angleBetweenTest() // TODO: Implement also
+    {
+        Vector2D a = new Vector2D(1, 1);
+        Vector2D b = new Vector2D(1, 0);
+        Vector2D c = new Vector2D(0, -1);
+        double actual = Vector2D.angleBetween(a, b);
+        assertEquals(Math.PI / 4, actual, delta);
+        actual = Vector2D.angleBetween(b, a);
+        assertEquals(Math.PI / 4, actual, delta);
+        actual = Vector2D.angleBetween(a, b.negate());
+        assertEquals(3 * Math.PI / 4, actual, delta);
+        actual = Vector2D.angleBetween(a, c);
+        assertEquals(3 * Math.PI / 4, actual, delta);
+    }
+    @Test
+    void equalsTest()
+    {
+        final double x = 1, y = 2;
+        Vector2D ones = new Vector2D(x, x);
+        Vector2D ones2 = new Vector2D(x, x);
+        Vector2D twoes = new Vector2D(y, y);
+        assertEquals(true, ones.equals(ones2));
+        assertEquals(false, ones.equals(twoes));
+    }
+    @Test
+    void equalsWithToleranceTest()
+    {
+        final double x = 1.55, y = 2.55;
+        Vector2D ones = new Vector2D(x, x);
+        Vector2D ones2 = new Vector2D(x, x);
+        Vector2D twoes = new Vector2D(y, y);
+        assertEquals(true, ones.equals(ones2, delta));
+        assertEquals(false, ones.equals(twoes, delta));
     }
 }
